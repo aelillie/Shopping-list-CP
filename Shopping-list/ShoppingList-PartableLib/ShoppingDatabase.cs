1c996cd31cdc6f0f49f4a07ebe4f47b36d2afc3c
Lillie <aedl@itu.dk>
2016-12-07 12:46:43 +0100

    init

---
+++ Shopping-list/ShoppingList-PartableLib/ShoppingDatabase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Realms;
+
+namespace ShoppingList_PartableLib
+{
+    public class ShoppingDatabase
+    {
+        private Realm _realm;
+        public static ShoppingDatabase ShoppingDb => ShoppingDb ?? new ShoppingDatabase();
+
+        private ShoppingDatabase()
+        {
+            _realm = Realm.GetInstance();
+        }
+
+
+        public IEnumerable<ShoppingItem> GetItems()
+        {
+            return _realm.All<ShoppingItem>().ToList();
+        }
+
+        public ShoppingItem GetItem(int id)
+        {
+            return _realm.All<ShoppingItem>().FirstOrDefault(); //TODO: Id og just name?
+        }
+    }
+}
---
+++ Shopping-list/ShoppingList-PartableLib/ShoppingItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Realms;
+
+namespace ShoppingList_PartableLib
+{
+    public class ShoppingItem : RealmObject
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+    }
+}
